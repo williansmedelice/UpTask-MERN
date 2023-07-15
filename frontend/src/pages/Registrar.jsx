43b6faa4e3e68becf86e9c081ab107912e951db4
Willians  Medelice Salazar <williansmedelicesalazar@Willianss-MacBook-Pro.local>
2023-07-14 23:03:28 -0400

    Agregando validaciones a formulario Registrar Usuarios

--- frontend/src/pages/Registrar.jsx
+++ frontend/src/pages/Registrar.jsx
@@ -1,19 +1,37 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import Alerta from "../components/Alerta";
 
 function Registrar() {
   const [nombre, setNombre] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [repetirPassword, setRepetirPassword] = useState("");
+  const [alerta, setAlerta] = useState({});
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if ([nombre, email, password, repetirPassword].includes("")) {
+      // console.log("Todos los campos son obligatorios");
+      setAlerta({
+        msg: "Todos los campos son obligatorios",
+        error: true,
+      });
+      return;
+    }
+  };
+  const { msg } = alerta;
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">
         Crea tu cuenta y administra tus {""}
         <span className="text-slate-700"> proyectos</span>
       </h1>
-      <form className="my-10 bg-white shadow rounded-lg p-10">
+      {msg && <Alerta alerta={alerta} />}
+      <form
+        className="my-10 bg-white shadow rounded-lg p-10"
+        onSubmit={handleSubmit}
+      >
         <div className="my-5">
           <label
             htmlFor="nombre"
