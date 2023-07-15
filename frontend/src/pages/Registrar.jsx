4fc27f00b87f9da06c9faf5ab51a5ee7f2bff8f4
Willians  Medelice Salazar <williansmedelicesalazar@Willianss-MacBook-Pro.local>
2023-07-14 23:14:37 -0400

    Validar en el formulario (Registro de Usuario) los campos (Password) y (Repetir Password) sean iguales

--- frontend/src/pages/Registrar.jsx
+++ frontend/src/pages/Registrar.jsx
@@ -19,6 +19,27 @@ function Registrar() {
       });
       return;
     }
+
+    if (password !== repetirPassword) {
+      setAlerta({
+        msg: "Los password no son iguales",
+        error: true,
+      });
+      return;
+    }
+
+    if (password.length < 6) {
+      setAlerta({
+        msg: "El Password es muy corto, agrega minimo 6 caracteres",
+        error: true,
+      });
+      return;
+    }
+
+    setAlerta({});
+
+    // Crear el usuario en la API
+    console.log("Creando...");
   };
   const { msg } = alerta;
   return (
