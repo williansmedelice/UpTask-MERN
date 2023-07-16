41549a9da87ec7bfd368a8bbaf0d74a98da67b1d
Willians  Medelice Salazar <williansmedelicesalazar@Willianss-MacBook-Pro.local>
2023-07-15 21:37:53 -0400

    Confirmando cuenta via Token

--- frontend/src/pages/ConfirmaCuenta.jsx
+++ frontend/src/pages/ConfirmaCuenta.jsx
@@ -1,10 +1,59 @@
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import axios from "axios";
+import Alerta from "../components/Alerta";
+
 const ConfirmaCuenta = () => {
+  const [alerta, setAlerta] = useState({});
+  const [cuentaConfirmada, setCuentaConfirmada] = useState(false);
+
+  const params = useParams();
+  const { token } = params;
+
+  useEffect(() => {
+    const confirmarCuenta = async () => {
+      try {
+        const url = `${
+          import.meta.env.VITE_BACKEND_URL
+        }/api/usuarios/confirmar/${token}`;
+
+        const { data } = await axios(url);
+        console.log(data);
+        setAlerta({
+          msg: data.msg,
+          error: false,
+        });
+        setCuentaConfirmada(true);
+      } catch (error) {
+        console.log(error);
+        setAlerta({
+          msg: error.response.data.msg,
+          error: true,
+        });
+      }
+    };
+    confirmarCuenta();
+  }, []);
+
+  const { msg } = alerta;
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">
         Confirma tu cuenta y comienza a crear tus {""}
         <span className="text-slate-700"> proyectos</span>
       </h1>
+      <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
+        {msg && <Alerta alerta={alerta} />}
+        {cuentaConfirmada && (
+          <Link
+            to="/"
+            className="block text-center my-5 text-slate-500 uppercase text-sm"
+          >
+            Inicia Sesión
+          </Link>
+        )}
+      </div>
     </>
   );
 };
