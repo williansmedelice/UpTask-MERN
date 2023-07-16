604fe9df971146afb41ea5488cc52212057aaac4
Willians  Medelice Salazar <williansmedelicesalazar@Willianss-MacBook-Pro.local>
2023-07-16 15:04:33 -0400

    Crear Lógica para revisar el Token para autenticar el usuario

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -1,9 +1,37 @@
 import { useState, useEffect, createContext } from "react";
+import clientAxios from "../config/clientAxios";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState({});
+
+  useEffect(() => {
+    const autenticarUsuario = async () => {
+      const token = localStorage.getItem("token");
+      //   console.log(token);
+      if (!token) {
+        return;
+      }
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      try {
+        const { data } = await clientAxios(`/usuarios/perfil`, config);
+        console.log(data);
+        setAuth(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    autenticarUsuario();
+  }, []);
   return (
     <AuthContext.Provider value={{ setAuth }}>{children}</AuthContext.Provider>
   );
