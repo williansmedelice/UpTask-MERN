52878310eda022e0bd89cc509141a5ca2297462b
Willians  Medelice Salazar <williansmedelicesalazar@Willianss-MacBook-Pro.local>
2023-07-15 22:38:00 -0400

    Creación de lógica para validación de formulario (Olvide Password)

--- frontend/src/pages/OlvidePassword.jsx
+++ frontend/src/pages/OlvidePassword.jsx
@@ -1,13 +1,36 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import Alerta from "../components/Alerta";
 
 function OlvidePassword() {
+  const [email, setEmail] = useState("");
+  const [alerta, setAlerta] = useState({});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (email === "" || email.length < 6) {
+      setAlerta({
+        msg: "El Email es obligatorio",
+        error: true,
+      });
+      return;
+    }
+  };
+
+  const { msg } = alerta;
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">
         Recupera tu acceso y no pierdas tus {""}
         <span className="text-slate-700"> proyectos</span>
       </h1>
-      <form className="my-10 bg-white shadow rounded-lg p-10">
+      {msg && <Alerta alerta={alerta} />}
+      <form
+        className="my-10 bg-white shadow rounded-lg p-10"
+        onSubmit={handleSubmit}
+      >
         <div className="my-5">
           <label
             htmlFor="email"
@@ -20,6 +43,8 @@ function OlvidePassword() {
             type="email"
             placeholder="Email del usuario"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <input
