915dee7f8e3065bf82d98c8124db8fd952c8f2b8
Willians  Medelice Salazar <williansmedelicesalazar@Willianss-MacBook-Pro.local>
2023-07-16 00:28:10 -0400

    Integración de logica para la validación del Token para resetear el Password

--- frontend/src/pages/NuevoPassword.jsx
+++ frontend/src/pages/NuevoPassword.jsx
@@ -1,31 +1,111 @@
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import axios from "axios";
+import Alerta from "../components/Alerta";
+
 function NuevoPassword() {
+  const [password, setPassword] = useState("");
+  const [alerta, setAlerta] = useState({});
+  const [tokenValido, setTokenValido] = useState(false);
+
+  const params = useParams();
+  const { token } = params;
+
+  useEffect(() => {
+    const comprobarToken = async () => {
+      try {
+        // TODO: Mover hacia un cliente Axios
+        await axios(
+          `${
+            import.meta.env.VITE_BACKEND_URL
+          }/api/usuarios/olvide-password/${token}`
+        );
+        setTokenValido(true);
+      } catch (error) {
+        // console.log(error);
+        setAlerta({
+          msg: error.response.data.msg,
+          error: true,
+        });
+      }
+    };
+    comprobarToken();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (password === "" || password.length < 6) {
+      setAlerta({
+        msg: "El Password es obligatorio",
+        error: true,
+      });
+      return;
+    }
+
+    try {
+      // TODO: Mover hacia un cliente Axios
+      const { data } = await axios.post(
+        `${
+          import.meta.env.VITE_BACKEND_URL
+        }/api/usuarios/olvide-password/${token}`,
+        { password }
+      );
+
+      console.log(data);
+
+      setAlerta({
+        msg: data.msg,
+        error: false,
+      });
+      setPassword("");
+    } catch (error) {
+      console.log(error.response);
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true,
+      });
+    }
+  };
+
+  const { msg } = alerta;
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">
         Reestablece tu password y no pierdas acceso a tus {""}
         <span className="text-slate-700"> proyectos</span>
       </h1>
-      <form className="my-10 bg-white shadow rounded-lg p-10">
-        <div className="my-5">
-          <label
-            htmlFor="password"
-            className="uppercase text-gray-600 block text-xl font-bold"
-          >
-            Nuevo Password
-          </label>
+      {msg && <Alerta alerta={alerta} />}
+
+      {tokenValido && (
+        <form
+          className="my-10 bg-white shadow rounded-lg p-10"
+          onSubmit={handleSubmit}
+        >
+          <div className="my-5">
+            <label
+              htmlFor="password"
+              className="uppercase text-gray-600 block text-xl font-bold"
+            >
+              Nuevo Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              placeholder="Escribe tu Nuevo Password"
+              className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
           <input
-            id="password"
-            type="password"
-            placeholder="Escribe tu Nuevo Password"
-            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            type="submit"
+            value="Guardar Nuevo Password"
+            className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
           />
-        </div>
-        <input
-          type="submit"
-          value="Guardar Nuevo Password"
-          className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
-        />
-      </form>
+        </form>
+      )}
     </>
   );
 }
