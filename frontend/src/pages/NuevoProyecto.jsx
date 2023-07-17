3e86e06f4cebadd71c624c29343c6d59a60b7ff3
Willians  Medelice Salazar <williansmedelicesalazar@Willianss-MacBook-Pro.local>
2023-07-17 06:38:46 -0400

    Creación formulario (Crear Proyecto)

--- frontend/src/pages/NuevoProyecto.jsx
+++ frontend/src/pages/NuevoProyecto.jsx
@@ -1,7 +1,12 @@
+import FormularioProyecto from "../components/FormularioProyecto";
+
 function NuevoProyecto() {
   return (
     <>
-      <h1 className="text-4xl font-black">Nuevo Proyecto</h1>
+      <h1 className="text-4xl font-black">Crear Proyecto</h1>
+      <div className="mt-10 flex justify-center">
+        <FormularioProyecto />
+      </div>
     </>
   );
 }
